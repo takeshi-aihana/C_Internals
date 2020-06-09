e819580628a7fd88387d57cd5e3dffef884f7d7e
t_aihana <t-aihana@core.co.jp>
2020-06-09 15:29:46 +0900

    * Updated translations and added codes.

---
+++ codes/sample13.s
@@ -0,0 +1,42 @@
+	.file	"sample13.c"
+	.text
+	.globl	fun
+	.type	fun, @function
+fun:
+.LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$16, %esp
+	movl	$0, -4(%ebp)
+	addl	$1, -4(%ebp)
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	fun, .-fun
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	call	fun
+	popl	%ebp
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 4.8.5-4ubuntu8) 4.8.5"
+	.section	.note.GNU-stack,"",@progbits
---
+++ codes/sample14.s
@@ -0,0 +1,45 @@
+	.file	"sample14.c"
+	.text
+	.globl	fun
+	.type	fun, @function
+fun:
+.LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	addl	$1, 8(%ebp)
+	movl	12(%ebp), %eax
+	addl	%eax, 8(%ebp)
+	popl	%ebp
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	fun, .-fun
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$8, %esp
+	movl	$3, 4(%esp)
+	movl	$2, (%esp)
+	call	fun
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 4.8.5-4ubuntu8) 4.8.5"
+	.section	.note.GNU-stack,"",@progbits
