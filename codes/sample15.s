6b24e17f3255b5d682cce3f1e8621ddd9b5e67c3
Takeshi AIHANA <takeshi.aihana@gmail.com>
2020-06-10 19:03:40 +0900

    * Updated translation and added code.

---
+++ codes/sample15.s
@@ -0,0 +1,43 @@
+	.file	"sample15.c"
+	.text
+	.globl	fun
+	.type	fun, @function
+fun:
+.LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	movl	$16, %eax
+	popl	%ebp
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	fun, .-fun
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$16, %esp
+	call	fun
+	movl	%eax, -4(%ebp)
+	movl	$0, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 4.8.5-4ubuntu8) 4.8.5"
+	.section	.note.GNU-stack,"",@progbits
